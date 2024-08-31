7801199d1d167a0ccd8ef62d4ad6af96aadc6225
jon-jh <hiebert456@gmail.com>
2024-08-31 17:36:31 -0600

    add objects-class-tree and methods for vampire

--- vampire.js
+++ vampire.js
@@ -10,21 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let count = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) { // while THIS.creator is not null (it will run until it reaches the root)
+      count++;
+      currentVampire = currentVampire.creator; // ADD 1, set THIS.creator as CURRENT, keep counting.
+    }
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
 
   }
 
